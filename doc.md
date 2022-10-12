35e9897cae6d7cbab2a44413d67a366a27b3c41a
Brandon Fernandez Mejia <brandonprogamer02@gmail.com>
2022-10-11 21:00:40 -0400

    docker-compose added

---
+++ doc.md
@@ -0,0 +1,11 @@
+# generate a new nest project
+
+nest new <project-name>
+
+# generate new class
+
+nest generate class <path>
+
+# generate new module
+
+nest generate module <path>
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { TuitsModule } from "./modules/tuits/tuits.module";
 
 @Module({
-  imports: [TuitsModule],
+  imports: [
+    TuitsModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "root",
+      database: "tuiter",
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
