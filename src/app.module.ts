815763632f460149cf8dd3d7891a1bc104d16fe8
Brandon Fernandez Mejia <brandonprogamer02@gmail.com>
2022-10-12 10:24:15 -0400

    docker added & .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,23 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TuitsModule } from "./modules/tuits/tuits.module";
 import { UserModule } from "./modules/user/user.module";
+import { DatabaseModule } from "./database/database.module";
 
 @Module({
   imports: [
-    TuitsModule,
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "root",
-      database: "tuiter",
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+    TuitsModule,
     UserModule,
+    DatabaseModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number;
+  constructor(private readonly configService: ConfigService) {
+    AppModule.port = +this.configService.get("PORT");
+  }
+}
---
+++ src/database/database.provider.ts
@@ -0,0 +1,26 @@
+import { DynamicModule } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConnectionOptions } from "typeorm";
+import { Environment } from "../common/enum";
+
+export const DatabaseProvider: DynamicModule = TypeOrmModule.forRootAsync({
+  inject: [ConfigService],
+  useFactory: async (config: ConfigService) => {
+    const isDevEnv = config.get("NODE_ENV") !== Environment.PRODUCTION;
+    const dbConfig = {
+      type: config.get("DB_TYPE"),
+      host: config.get("DB_HOST"),
+      port: +config.get("DB_PORT"),
+      username: config.get("DB_USERNAME"),
+      password: config.get("DB_PASSWORD"),
+      database: config.get("DB_DATABASE"),
+      ...(config.get("DB_LOGGING") === "true" ? { logging: true } : {}),
+
+      autoLoadEntities: true,
+      synchronize: isDevEnv,
+    } as ConnectionOptions;
+
+    return dbConfig;
+  },
+});
